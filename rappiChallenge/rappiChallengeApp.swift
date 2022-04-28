a77cbd46d511b90d6a19b1e0e174512104305063
Gustavo Molluso <gmolluso@gmail.com>
2022-04-28 09:14:49 -0300

    Initial Commit

---
+++ rappiChallenge/rappiChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  rappiChallengeApp.swift
+//  rappiChallenge
+//
+//  Created by Gustavo Molluso on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct rappiChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rappiChallengeUITests/rappiChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rappiChallengeUITestsLaunchTests.swift
+//  rappiChallengeUITests
+//
+//  Created by Gustavo Molluso on 28/04/2022.
+//
+
+import XCTest
+
+class rappiChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
