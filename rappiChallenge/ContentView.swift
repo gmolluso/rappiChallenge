64688c0cc4d25847018587a0ebef3076b822b192
Gustavo Molluso <gmolluso@gmail.com>
2022-05-03 15:19:17 -0300

    initial Recover from deletion

--- rappiChallenge/ContentView.swift
+++ rappiChallenge/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+//        FIXME: Todas las clases States las podemos pasar a una unica con las 3 funciones
     }
 }
 
